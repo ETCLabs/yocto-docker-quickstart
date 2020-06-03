555bff10f968991d7cb33811df2a24ea22d9ae85
Zach Ferree <ferree.zach@gmail.com>
2020-06-03 13:56:26 -0500

    update clone-yocto.sh
    changing the clone verion of repos to warrior not dunfell

--- clone-yocto.sh
+++ clone-yocto.sh
@@ -22,10 +22,10 @@ fi
 mkdir -p "${directory}/sources" && cd "${directory}"
 
 # clone poky and other layers
-git clone -b dunfell git://git.yoctoproject.org/poky.git sources/poky
-git clone -b dunfell https://github.com/Freescale/meta-freescale.git sources/meta-freescale
-git clone -b dunfell https://github.com/Freescale/meta-freescale-3rdparty.git sources/meta-freescale-3rdparty
-git clone -b dunfell https://github.com/Freescale/meta-freescale-distro.git sources/meta-freescale-distro
-git clone -b dunfell https://github.com/openembedded/meta-openembedded.git sources/meta-openembedded
+git clone -b warrior git://git.yoctoproject.org/poky.git sources/poky
+git clone -b warrior https://github.com/Freescale/meta-freescale.git sources/meta-freescale
+git clone -b warrior https://github.com/Freescale/meta-freescale-3rdparty.git sources/meta-freescale-3rdparty
+git clone -b warrior https://github.com/Freescale/meta-freescale-distro.git sources/meta-freescale-distro
+git clone -b warrior https://github.com/openembedded/meta-openembedded.git sources/meta-openembedded
 
 echo "Done, type \"cd ${directory} && . ./sources/poky/oe-init-build-env\" to create the build environment"
