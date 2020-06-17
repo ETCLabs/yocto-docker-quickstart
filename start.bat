74353676b02e01e34d5b8a6f3bb58412a2ca96f1
Sam <samuelmkearney@gmail.com>
2020-06-17 12:19:50 -0500

    Add explanatory comment in start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,11 @@
 @echo off
 
+REM We can't mount /opt/yocto or /opt/yocto/workspace as volumes in Windows, because this will make
+REM bitbake fail (it tries to create a UNIX domain socket with the path to its current directory,
+REM and for whatever reason that doesn't work in a mounted volume on Windows). So the mounts need
+REM to be a bit more granular, so that /opt/yocto/workspace/build is not a volume and only exists
+REM inside the container.
+
 docker container run ^
   -it ^
   --rm ^
